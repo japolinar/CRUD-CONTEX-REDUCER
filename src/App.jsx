ad627668c2df441c0c77fb6bf0e599c72d7eab4b
japolinar <japolinar_1@hotmail.com>
2023-06-07 11:39:52 -0400

    Logo React

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ function App() {
   return (
     <div className="container text-center">
       <div className="m-5 grady d-flex p-2 rounded justify-content-center">
-        <img src={viteLogo} alt="" className="px-3" />
+        <img src={viteLogo} alt="Logo Vite" className="px-3" />
 
         <h2 className="text-uppercase text-white px-2">
           CRUD Context - Reducer
         </h2>
+        <img src={reactLogo} alt="Logo React" className="px-3" />
       </div>
       <Create/>
       <Show/>
